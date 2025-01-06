7c1a11e67bf1bfc4ba070db011c95eed5258c68f
kelvin abche <kelvinabache12@gmail.com>
2025-01-06 18:17:43 -0400

    removiendo cosas de mas

--- db/driver/drive_db.mjs
+++
@@ -1,9 +0,0 @@
-export function drive_db(err) {
-  if (err) {
-    console.error(err);
-  }
-
-  console.log("All is goog");
-}
-
-// modify is code, for tha t is simple and function in each part of program 
